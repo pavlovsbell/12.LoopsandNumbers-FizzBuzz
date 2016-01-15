82e3430d822348f151bdd00a80f32a072c8b8f18
Julianne <juliannebagsh@gmail.com>
2016-01-15 04:26:06 +0000

    Initial Commit

---
+++ 12.LoopsandNumbers-FizzBuzz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  12.LoopsandNumbers-FizzBuzz
+//
+//  Created by Julianne on 15/01/2016.
+//  Copyright (c) 2016 Julianne. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
